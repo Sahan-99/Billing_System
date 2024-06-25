4fffdabd5f3405b646b4ee9c0cf39ba1599fbb3d
Sahan Sandaruwan <sahansandaruwan21@gmail.com>
2024-06-25 13:19:12 +0530

    first commit

---
+++ Billing_System/src/main/java/com/mycompany/billing_system/Billing_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.billing_system;
+
+/**
+ *
+ * @author Sahan
+ */
+public class Billing_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
